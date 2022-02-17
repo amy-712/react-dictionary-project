b2a237f98d5caa409a98b61c3d008e829bca1831
amy-712 <91413690+amy-712@users.noreply.github.com>
2022-02-17 22:32:08 +0000

    Added Search Form and stylised components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return "Hello World";
+  return (
+    <div className="dictionaryAppWrapper">
+      <div className="dictionaryApp">
+        <h1>Dictionary</h1>
+        <Dictionary />
+      </div>
+    </div>
+  );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Search🕵🏼‍♀️"
+        />
+      </form>
+    </div>
+  );
+}
